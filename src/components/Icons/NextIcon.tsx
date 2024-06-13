698506d927b09090760b78100f95a8ea4aace179
Yauheni Kadyrau <7077634@gmail.com>
2024-06-13 21:51:33 +0300

    feat: add Date calendar, add select Day

---
+++ src/components/Icons/NextIcon.tsx
@@ -0,0 +1,49 @@
+interface NextProps {
+    className?: string;
+}
+
+export const NextIcon = ({ className }: NextProps) => (
+    <svg
+        className={className}
+        width="16"
+        height="16"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink">
+        <defs>
+            <clipPath id="clip1_14">
+                <rect
+                    id="Icon / Calendar / Next"
+                    width="16.000000"
+                    height="16.000000"
+                    fill="white"
+                    fillOpacity="0"
+                />
+            </clipPath>
+        </defs>
+        <rect
+            id="Icon / Calendar / Next"
+            width="16.000000"
+            height="16.000000"
+            fill="#FFFFFF"
+            fillOpacity="0"
+        />
+        <g clipPath="url(#clip1_14)">
+            <path
+                id="Vector"
+                d="M4.27 4L3.33 4.94L6.38 8L3.33 11.06L4.27 12L8.27 8L4.27 4Z"
+                fill="#000000"
+                fillOpacity="1.000000"
+                fillRule="nonzero"
+            />
+            <path
+                id="Vector"
+                d="M8.66 4L7.72 4.94L10.78 8L7.72 11.06L8.66 12L12.66 8L8.66 4Z"
+                fill="#000000"
+                fillOpacity="1.000000"
+                fillRule="nonzero"
+            />
+        </g>
+    </svg>
+);
---
+++ src/constants/constants.ts
@@ -0,0 +1,35 @@
+export const MONTH_NAMES = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+export enum StartDayOfWeek {
+    Monday = "Monday",
+    Sunday = "Sunday",
+}
+export const DAY_NAMES = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+];
+
+export const SHORT_DAY_NAMES = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+
+export const numbersArray: number[] = [];
+for (let i = 1; i <= 30; i += 1) {
+    numbersArray.push(i);
+}
